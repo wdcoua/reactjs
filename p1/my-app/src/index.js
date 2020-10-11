6962be2da43974fd3718a0e360d5fa8327e0237b
wd <47643023+wdcoua@users.noreply.github.com>
2020-10-11 20:15:00 +0300

    Update index.js

--- p1/my-app/src/index.js
+++ p1/my-app/src/index.js
@@ -5,11 +5,9 @@ import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-//import {Provider} from "./StoreContext";
 import {Provider}  from "react-redux";
 
 
-// let rerenderEntireTree = () => {
     ReactDOM.render(
         <BrowserRouter>
             <Provider store={store}>
@@ -18,32 +16,6 @@ import {Provider}  from "react-redux";
 
         </BrowserRouter>, document.getElementById('root')
     );
-// }
 
-//
-// rerenderEntireTree();
-//
-// store.subscribe(() => {
-//     let state = store.getState();
-//     rerenderEntireTree(state);
-// })
 
 serviceWorker.unregister();
-
-/**********/
-// wd@wd-nout:/media/wd/1c65ded6-8343-4eea-b5cb-6cb824d7acab/wd/wd5/webdev/react/p1/my-app$
-// npm start
-
-// карта-схема
-// https://app.lucidchart.com/documents/edit/eeddadd9-a7cb-444a-8750-5b07377a1d66/0_0?beaconFlowId=25B49DD164AED936#?folder_id=home&browser=icon
-
-
-/*
-
-console.log('before request')
-axios
-    .get('https://blabla.com/dfg')
-    .then(data => console.log(data))
-console.log('after request')
-
-* */
