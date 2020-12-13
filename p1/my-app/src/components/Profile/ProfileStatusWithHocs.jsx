6f5910090a33f0ebc37ab560f60c5c9f936d9d41
wdcoua <fkjjkh54kKJHds830298fKJH>
2020-12-13 16:13:24 +0200

    85 - React JS - hook, useEffect, хуки

--- p1/my-app/src/components/Profile/ProfileStatusWithHocs.jsx
+++ p1/my-app/src/components/Profile/ProfileStatusWithHocs.jsx
@@ -1,10 +1,19 @@
-import React, {useState} from "react"
+import React, {useState,useEffect} from "react"
 import {connect} from "react-redux";
+import {setStatus} from "../../redux/profile_reducer";
 
 const ProfileStatusWithHocs = (props) => {
 
+    /*
+    * хуки НЕ можна використовувати в умовах і циклах
+    * */
+
     let [editMode,setEditMode] = useState(false); // hook
     let [tempStatus,setTempStatus] = useState(props.status); // hook
+
+    useEffect( () => { // хук, функція, яка виконується ПІСЛЯ того як відбулось відображення рендерингу
+        setStatus(props.status) // що зробити
+    },[props.status]); // при якій умові - коли змінюється вказаний елемент // якщо умова - порожній масив - [] - то поведінка буде як у componentDidMount
 
     const toggleEditMode = () => {
         if( editMode === false)
