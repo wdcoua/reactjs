605183798e89019fef23d5bce6c2cf52aed00655
wdcoua <fkjjkh54kKJHds830298fKJH>
2020-12-13 22:03:52 +0200

    87 - shouldComponentUpdate, PureComponent, memo - React JS

--- p1/my-app/src/components/Profile/Profile.jsx
+++ p1/my-app/src/components/Profile/Profile.jsx
@@ -1,8 +1,11 @@
+// import React, {Component} from "react";
+// import React, {PureComponent} from "react";
 import React from "react";
 
 
 
-const Profile = (props) => {
+
+const Profile = React.memo((props) => {
     return <div>
         <img src={props.profile.photos.large} alt=""/>
         <div><b>{props.profile.fullName}</b></div>
@@ -15,7 +18,85 @@ const Profile = (props) => {
         <div>{props.profile.contacts.vk}</div>
 
     </div>;
+});
+
+/*
+
+дублюю верхнє
+
+
+
+const Profile = React.memo((props) => {
+    return <div>
+        <img src={props.profile.photos.large} alt=""/>
+        <div><b>{props.profile.fullName}</b></div>
+        <div>Статус: {props.status}</div>
+        <div>Обо мне: "{props.profile.aboutMe}"</div><br/>
+        Мои контакты: <br/>
+        <div>{props.profile.contacts.facebook}</div>
+        <div>{props.profile.contacts.github}</div>
+        <div>{props.profile.contacts.instagram}</div>
+        <div>{props.profile.contacts.vk}</div>
+
+    </div>;
+});
+
+
+*/
+
+/*
+або
+
+class Profile extends PureComponent {
+
+    render() {
+        return <div>
+            <img src={this.props.profile.photos.large} alt=""/>
+            <div><b>{this.props.profile.fullName}</b></div>
+            <div>Статус: {this.props.status}</div>
+            <div>Обо мне: "{this.props.profile.aboutMe}"</div><br/>
+            Мои контакты: <br/>
+            <div>{this.props.profile.contacts.facebook}</div>
+            <div>{this.props.profile.contacts.github}</div>
+            <div>{this.props.profile.contacts.instagram}</div>
+            <div>{this.props.profile.contacts.vk}</div>
+
+        </div>;
+    }
 }
+
+*/
+
+
+/*
+
+або
+
+
+class Profile extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps != this.props || nextState != this.state;
+    }
+
+    render() {
+        return <div>
+            <img src={this.props.profile.photos.large} alt=""/>
+            <div><b>{this.props.profile.fullName}</b></div>
+            <div>Статус: {this.props.status}</div>
+            <div>Обо мне: "{this.props.profile.aboutMe}"</div><br/>
+            Мои контакты: <br/>
+            <div>{this.props.profile.contacts.facebook}</div>
+            <div>{this.props.profile.contacts.github}</div>
+            <div>{this.props.profile.contacts.instagram}</div>
+            <div>{this.props.profile.contacts.vk}</div>
+
+        </div>;
+    }
+}
+
+* */
+
 
 
 export default Profile
--- p1/my-app/src/redux/users_reducer.js
+++ p1/my-app/src/redux/users_reducer.js
@@ -125,7 +125,7 @@ export const setFollowingInProgress = (followingIsInProgress,userId) => {
 
 
 // thunk-и
-export const getUsers = (usersPerPage = 20,currentPage = 85) => {
+export const getUsers = (usersPerPage = 20,currentPage = 86) => {
     return (dispatch) => {
 
         dispatch(setFetchingStatus(true));
