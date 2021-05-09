755f09d6fe831fbd2a27034afb4e9e92993c5ed7
wdcoua <wd.co.ua@gmail.com>
2021-05-10 01:30:36 +0300

    deploy111

---
+++ p1/my-app/build/precache-manifest.5cd1a858c8c0ae132de7747e6d4b0845.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e8318b480d6686a3e541df2c44b31e",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "98af5537449b7309966b",
+    "url": "/react/static/css/10.57f62c54.chunk.css"
+  },
+  {
+    "revision": "75ac36bc5c77be9b2972",
+    "url": "/react/static/css/4.75579d9c.chunk.css"
+  },
+  {
+    "revision": "dd96838e0ffad5ee9456",
+    "url": "/react/static/css/5.b05c24aa.chunk.css"
+  },
+  {
+    "revision": "f9847785b4484bea187c",
+    "url": "/react/static/css/6.4fb062a2.chunk.css"
+  },
+  {
+    "revision": "e3515394a16711de1cc9",
+    "url": "/react/static/css/7.12bae9c1.chunk.css"
+  },
+  {
+    "revision": "d0ffd1606edc7a5d1c88",
+    "url": "/react/static/css/9.93c08bb5.chunk.css"
+  },
+  {
+    "revision": "6ee26c6ffa3527c2504a",
+    "url": "/react/static/css/main.4f986e7c.chunk.css"
+  },
+  {
+    "revision": "6c89ef4681769ae33992",
+    "url": "/react/static/js/0.8308718c.chunk.js"
+  },
+  {
+    "revision": "98af5537449b7309966b",
+    "url": "/react/static/js/10.a5e96bd8.chunk.js"
+  },
+  {
+    "revision": "fdc20f5f0b69f8b10956",
+    "url": "/react/static/js/3.ccafee16.chunk.js"
+  },
+  {
+    "revision": "4f2dedb9f87ffe36ce515bb1189ae74f",
+    "url": "/react/static/js/3.ccafee16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ac36bc5c77be9b2972",
+    "url": "/react/static/js/4.5d5de05e.chunk.js"
+  },
+  {
+    "revision": "dd96838e0ffad5ee9456",
+    "url": "/react/static/js/5.b0888014.chunk.js"
+  },
+  {
+    "revision": "f9847785b4484bea187c",
+    "url": "/react/static/js/6.f6c9a655.chunk.js"
+  },
+  {
+    "revision": "e3515394a16711de1cc9",
+    "url": "/react/static/js/7.362989c1.chunk.js"
+  },
+  {
+    "revision": "ac4e3c367bf5e20a0e14",
+    "url": "/react/static/js/8.949b144d.chunk.js"
+  },
+  {
+    "revision": "d0ffd1606edc7a5d1c88",
+    "url": "/react/static/js/9.f14e29a4.chunk.js"
+  },
+  {
+    "revision": "6ee26c6ffa3527c2504a",
+    "url": "/react/static/js/main.98525033.chunk.js"
+  },
+  {
+    "revision": "84347cca86d0a0a7acb8",
+    "url": "/react/static/js/runtime-main.38fb204e.js"
+  },
+  {
+    "revision": "70d84a677888ee6f771f499e10ee31b8",
+    "url": "/react/static/media/kotigoroshko.70d84a67.jpg"
+  },
+  {
+    "revision": "264a22fb2eb049accdb80d3fd6933201",
+    "url": "/react/static/media/logout.264a22fb.png"
+  },
+  {
+    "revision": "5fe3cf7c38e9fb6870907a69dda01c56",
+    "url": "/react/static/media/rings.5fe3cf7c.svg"
+  },
+  {
+    "revision": "d961d73a3082870d7e90b441c30152cf",
+    "url": "/react/static/media/user.d961d73a.png"
+  },
+  {
+    "revision": "565b495082b4fea89a856822904d3979",
+    "url": "/react/static/media/zloy_admin.565b4950.png"
+  }
+]);
